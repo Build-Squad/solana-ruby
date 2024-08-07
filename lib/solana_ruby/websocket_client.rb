77e4066a6c03f40a2b184e04c9395f070da6cbaf
chinnabellamkonda <china.bellamkonda@navtech.io>
2024-08-07 17:46:23 +0530

    Websocket client message scope update to keep the script running to continue receiving updates

--- lib/solana_ruby/websocket_client.rb
+++ lib/solana_ruby/websocket_client.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 require 'websocket-client-simple'
 require 'securerandom'
 require 'json'
 require 'pry'
-require_relative 'websocket_client_helper'
 
 module SolanaRuby
   class WebsocketClient
     def initialize(url)
       @url = url
-      @subscriptions = {}
+      @@subscriptions = {}
       @ws = WebSocket::Client::Simple.connect(@url)
 
       setup_handlers
@@ -16,7 +17,7 @@ module SolanaRuby
 
     def subscribe(method, params = nil, &block)
       id = generate_id
-      @subscriptions[id] = block
+      @@subscriptions[id] = block
       message = {
         jsonrpc: '2.0',
         id: id,
@@ -42,16 +43,24 @@ module SolanaRuby
 
     def setup_handlers
       @ws.on :message do |msg|
-        ws_client_helper = SolanaRuby::WebsocketClientHelper.new
-        ws_client_helper.handle_message(msg.data)
+        data = JSON.parse(msg.data)
+        if data['id'] && @@subscriptions[data['id']]
+          @@subscriptions[data['id']].call(data['result'])
+        elsif data['method'] && data['params']
+          @@subscriptions.each do |id, block|
+            block.call(data['params']) if block
+          end
+        else
+          puts "Unhandled message: #{msg.data}"
+        end
       end
 
       @ws.on :open do
-        puts 'Connected'
+        puts 'Websocket connection established.'
       end
 
       @ws.on :close do |e|
-        puts "Connection closed: #{e.inspect}"
+        puts "Websocket connection closed: #{e.inspect}"
       end
 
       @ws.on :error do |e|
@@ -68,17 +77,17 @@ end
 
 # client = SolanaRuby::WebsocketClient.new("wss://api.devnet.solana.com")
 
-# account_pubkey = ""
+# account_pubkey = "9B5XszUGdMaxCZ7uSQhPzdks5ZQSmWxrmzCSvtJ6Ns6g"
 
 # # Subscribe to account updates
-# subscription_id = client.subscribe("rootSubscribe") do |message|
-#   puts "Received slot update: #{message}"
+# subscription_id = client.subscribe("slotSubscribe") do |message|
+#   puts "The updates is: #{message}"
 # end
 
 # # Simulate running for a while to receive messages
-# sleep(60)
+# sleep(120)
 
 # # Unsubscribe
-# client.unsubscribe("rootUnsubscribe", subscription_id)
+# client.unsubscribe("slotSubscribe", subscription_id)
 
 
