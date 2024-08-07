21974895ff997b6fcd02081396a8b669fe9f76c6
chinnabellamkonda <china.bellamkonda@navtech.io>
2024-08-07 18:28:19 +0530

    adding get_balance, get_balance_and_context, get_account_info, and get_account_info_and_context http methods

--- lib/solana_ruby/http_methods/basic_methods.rb
+++ lib/solana_ruby/http_methods/basic_methods.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 module SolanaRuby
   module HttpMethods
     module BasicMethods
 
       def get_balance(pubkey)
-        request("getBalance", [pubkey])
+        balance_info = request("getBalance", [pubkey])
+        balance_info["result"]["value"]
       end
 
-      def get_recent_blockhash
-        request("getRecentBlockhash")
+      def get_balance_and_context(pubkey)
+        balance_info = request("getBalance", [pubkey])
+        balance_info["result"]
+      end
+
+      def get_account_info(pubkey)
+        account_info = request("getAccountInfo", [pubkey])
+        account_info["result"]["value"]
+      end
+
+      def get_account_info_and_context(pubkey)
+        account_info = request("getAccountInfo", [pubkey])
+        account_info["result"]
       end
     end
   end
